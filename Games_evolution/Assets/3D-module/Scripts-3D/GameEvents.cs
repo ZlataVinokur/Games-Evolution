ca2bbdd326f69025a5b6c4429c2c0e127b88e02e
ZlataVinokur <zlatapeach7@gmail.com>
2026-05-18 05:49:07 +0300

    triggers

--- Games_evolution/Assets/3D-module/Scripts-3D/GameEvents.cs
+++ Games_evolution/Assets/3D-module/Scripts-3D/GameEvents.cs
@@ -3,15 +3,70 @@ using UnityEngine.SceneManagement;
 
 public class GameEvents : MonoBehaviour
 {
+    public EncyclopediaOnMechanics encyclopedia; // Ссылка на скрипт энциклопедии
+
     private void Start()
     {
-        Debug.Log($" GameEvents инициализирован на объекте {gameObject.name}");
+        Debug.Log($"GameEvents инициализирован на объекте {gameObject.name}");
+
+        // Если ссылка не назначена в инспекторе, попробуем найти автоматически
+        if (encyclopedia == null)
+        {
+            encyclopedia = FindObjectOfType<EncyclopediaOnMechanics>();
+        }
     }
 
     public void OnGameWin()
     {
-
         GameManager.Instance.LoadQuizForCurrentModule(8);
+    }
 
+    // Методы для триггеров
+    public void TriggerMovementFact()
+    {
+        if (encyclopedia != null && !UnifiedInfoSystem.Instance.IsShowingAnything())
+        {
+            encyclopedia.ShowMovementFact();
+        }
+    }
+
+    public void TriggerLightFact()
+    {
+        if (encyclopedia != null && !UnifiedInfoSystem.Instance.IsShowingAnything())
+        {
+            encyclopedia.ShowLightFact();
+        }
+    }
+
+    public void TriggerLightFact2()
+    {
+        if (encyclopedia != null && !UnifiedInfoSystem.Instance.IsShowingAnything())
+        {
+            encyclopedia.ShowLightFact2();
+        }
+    }
+
+    public void TriggerCameraFact()
+    {
+        if (encyclopedia != null && !UnifiedInfoSystem.Instance.IsShowingAnything())
+        {
+            encyclopedia.ShowCameraFact();
+        }
+    }
+
+    public void TriggerPlatformerFact()
+    {
+        if (encyclopedia != null && !UnifiedInfoSystem.Instance.IsShowingAnything())
+        {
+            encyclopedia.ShowPlatformerFact();
+        }
+    }
+
+    public void TriggerEndFact()
+    {
+        if (encyclopedia != null && !UnifiedInfoSystem.Instance.IsShowingAnything())
+        {
+            encyclopedia.ShowEndFact();
+        }
     }
 }
